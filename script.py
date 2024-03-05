808c6271a5873b5f8b825d0b1edfc6ded97c4a77
bamnly <162134933+bamnly@users.noreply.github.com>
2024-03-05 14:24:42 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-# Download the new-bg
+# ========== Download the new-bg ==========
 import requests
 
 # URL of the file to download
@@ -18,9 +18,7 @@ else:
 
 
 
-
-
-# Set up the new-bg
+# ========== Set up the new-bg ==========
 import ctypes
 import os
 
@@ -36,3 +34,43 @@ def set_wallpaper(image_path):
 
 # Call the function with the path to the downloaded image
 set_wallpaper("new-bg.png")
+
+
+
+
+
+# ========== Download and launch music ==========
+
+import os
+import platform
+import pygame
+import requests
+
+def play_sound(sound_file):
+    if platform.system() == "Windows":
+        pygame.mixer.init()
+        pygame.mixer.music.load(sound_file)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+    elif platform.system() == "Darwin":
+        os.system(f"afplay '{sound_file}'")  # macOS
+    elif platform.system() == "Linux":
+        os.system(f"aplay {sound_file}")  # Linux
+    else:
+        print("Unsupported operating system.")
+
+def download_sound(url, filename):
+    response = requests.get(url)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+if __name__ == "__main__":
+    sound_file = "tu-tu-tu-du-max-verstappen.mp3"
+    url = "https://github.com/bamnly/wallpaper-changer/raw/main/sounds/tu-tu-tu-du-max-verstappen.mp3"
+
+    # Download the sound file if it doesn't exist
+    if not os.path.exists(sound_file):
+        download_sound(url, sound_file)
+
+    play_sound(sound_file)
